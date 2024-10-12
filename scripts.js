c1a23beb4d05bcd77aaa461a8df8f59a99369592
Ronald Magno <rrmagno@joyride.com.ph>
2024-10-12 22:05:35 +0800

    Add modal for dress code

--- scripts.js
+++ scripts.js
@@ -152,9 +152,22 @@ document.addEventListener("DOMContentLoaded", () => {
     $('#bgndVideo').YTPlayer();
   });
   
-  // window.addEventListener('scroll', function() {
-  //   const parallax = document.querySelector('.section-rsvp');
-  //   let scrollPosition = window.pageYOffset;
-  //   parallax.style.backgroundPositionY = (scrollPosition * 0.5) + 'px';
-  // });
+  document.addEventListener("DOMContentLoaded", function () {
+    var modal = document.getElementById("myModal");
+    var btn = document.getElementById("openModal");
+    var span = document.getElementsByClassName("close")[0];
   
+    btn.onclick = function () {
+      modal.style.display = "block";
+    };
+  
+    span.onclick = function () {
+      modal.style.display = "none";
+    };
+  
+    window.onclick = function (event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    };
+  });
